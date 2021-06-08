3fd86ddff8749749997bab3eb57cd363669207f0
asherhy <hongyongrs@gmail.com>
2021-06-09 04:02:27 +0800

    Auth Relations
    1) created 3 roles : student, mentor, admin
    
    2) added in middleware authentication for authentication purpose when deciding which routes can be authorized by which roles.
    
    3) Shifted back to Mysql and vuejs
    
    4) established relationship between roles and users.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
@@ -26,5 +27,18 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         //
+        Gate::define('admin-priv', function(){
+            return Auth::user()->allRoles()->contains('admin');
+          });
+        
+        Gate::define('student-priv', function(){
+            return Auth::user()->allRoles()->contains('student');
+
+        });
+
+        Gate::define('mentor-priv', function(){
+            return Auth::user()->allRoles()->contains('mentor');
+
+        });
     }
 }
--- app/User.php
+++ app/User.php
@@ -3,7 +3,8 @@
 namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Jenssegers\Mongodb\Auth\User as Authenticatable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+//use Jenssegers\Mongodb\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
@@ -36,4 +37,22 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class)->withTimestamps();
+    }
+
+    public function assignRole($role)
+    {
+        if(is_string($role)) {
+            $role = Role::whereName($role)->firstOrFail();
+        }
+        $this->roles()->sync($role, false);
+    }
+
+    public function allRoles()
+    {
+        return $this->roles->map->name;
+    }
 }
