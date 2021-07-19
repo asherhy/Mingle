19dd770701c4a51fe2f2bc44af077fe9c0b87f3d
asherhy <hongyongrs@gmail.com>
2021-07-19 15:13:08 +0800

    Added profile photo function
    1)Create a symlink.
    
    2) file validation for image types and size
    3)Store the image.
    4)Generate a url and store it in DB so i will be able to display it into User

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'telegram', 'gender', 'matric_year'
+        'name', 'email', 'password', 'telegram', 'gender', 'matric_year', 'avatar'
     ];
 
     /**
