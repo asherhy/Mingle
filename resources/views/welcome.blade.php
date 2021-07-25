95d4e4e669161e27f39b3ac73c23ef2a57f13c3b
asherhy <hongyongrs@gmail.com>
2021-07-25 23:36:29 +0800

    centered welcome page
    centered welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,7 +24,7 @@
 
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#00cccc" fill-opacity="1" d="M0,224L48,218.7C96,213,192,203,288,176C384,149,480,107,576,96C672,85,768,107,864,133.3C960,160,1056,192,1152,170.7C1248,149,1344,75,1392,37.3L1440,0L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
 <div class="container-fluid" id="section2" style="background-color: #00cccc;">
-    <div class="row ml-5 welcome-page">
+    <div class="row ml-5 mr-5 welcome-page">
         <div class="col-md-12">
             <div  class="d-flex justify-content-center">
                 <h1 class="text-light">Connecting you<br>with the people<br> you need</h1>
@@ -42,7 +42,7 @@
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#00cccc" fill-opacity="1" d="M0,320L48,288C96,256,192,192,288,149.3C384,107,480,85,576,90.7C672,96,768,128,864,160C960,192,1056,224,1152,208C1248,192,1344,128,1392,96L1440,64L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
 
 <div class="container-fluid" id="about">
-    <div class="row ml-5 welcome-page">
+    <div class="row ml-5 mr-5 welcome-page">
         <div class="col-md-12">
             <div  class="d-flex justify-content-center">
                 <h1 class="text-muted">Some Information<br> About Us...</h1>
@@ -66,7 +66,7 @@
 
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#00cccc" fill-opacity="1" d="M0,192L48,202.7C96,213,192,235,288,234.7C384,235,480,213,576,170.7C672,128,768,64,864,37.3C960,11,1056,21,1152,32C1248,43,1344,53,1392,58.7L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
 <div class="container-fluid" id="help" style="background-color: #00cccc;">
-    <div class="row ml-5 welcome-page">
+    <div class="row ml-5 mr-5 welcome-page">
         <div class="col-md-12">
             <div  class="d-flex justify-content-center">
                 <h1 class="text-light">Having trouble or have any<br> questions about the features? </h1>
